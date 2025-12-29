8d35b5591c3fad474532f96a97794896d769e351
SonnyBoyy <supernexor@gmail.com>
2025-12-29 22:26:27 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,28 +100,28 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Each completion must include a CPS, FPS counter and a cheat indicator.
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Each completion must be a level that is listed on the site.
                     </p>
                     <p>
                         Have either source audio or clicks/taps in the video. Edited audio only does not count
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt.
                     </p>
                     <p>
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Secret ways and swag routes are allowed IF AND ONLY IF: they don't skip more than 1% of the level; they make the level harder or don't change its difficulty.
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        The completion must be on the original, unmodified level that can be seen in the website. Check if the ID is correct before submitting.
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a level falls onto the Legacy List, records will still be accepted but they will not grant any points.
                     </p>
                 </div>
             </div>
